20cf09f163f5021cd5c67a7985adfcc58870948c
Robby <zhangrobby@yandex.com>
2021-09-17 19:28:47 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'App',
+  components: {
+    Navbar,
+  },
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <a class="navbar-brand" href="#"><b>Port</b>ofolio</a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link class="nav-link">About</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Contact</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
